ef41bdde59d63e9b5ff5f99ff90a4649be1b2d16
Paulo Maximialian <maxmilian@MacBook-Air-de-Paulo.local>
2025-12-24 22:30:16 -0300

    a

--- services/email.js
+++ services/email.js
@@ -7,7 +7,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true, // true para 465, false para outras portas
+    secure: process.env.SMTP_PORT == 465, // true para 465, false para outras portas
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
